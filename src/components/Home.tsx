2fe49834f9c6f5edbd20dcce6051e4eaf5d6b358
Igor Alves <igorservicosdev@gmail.com>
2024-06-21 10:15:45 -0300

    refactor: divide em componentes e hooks para facilitar legibilidade.

---
+++ src/components/Home.tsx
@@ -0,0 +1,89 @@
+import { useHome } from '@/hooks/useHome'
+import { VideoService } from '@/services/VideoService'
+import { Download, FileVideo, RotateCw } from 'lucide-react'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
+const videoService = new VideoService()
+
+export function Home() {
+  const {
+    getBgColor,
+    getLabel,
+    handleInit,
+    handleInput,
+    handleResetForm,
+    videosUrls,
+    inputFile,
+    videoRef,
+    videoUrlUnify,
+    files,
+  } = useHome(videoService)
+
+  return (
+    <main className="max-w-md md:max-w-xl p-4 mx-auto space-y-6">
+      <div className="space-y-px">
+        <FileVideo className="text-center w-full h-12" />
+        <h1 className="text-center text-md sm:text-2xl font-semibold">
+          Transforme seus vídeos em clipes de 1 minuto
+        </h1>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <div className="border p-6 rounded-md">
+          <Input ref={inputFile} type="file" multiple onChange={handleInput} />
+        </div>
+
+        <div className="flex items-center justify-between gap-1">
+          <Button
+            disabled={!files.length}
+            className={`w-full hover:${getBgColor()} ${getBgColor()} flex items-center`}
+            onClick={() => handleInit()}
+          >
+            {getLabel()}
+          </Button>
+
+          <Button
+            onClick={() => handleResetForm()}
+            className="w-full flex items-center"
+            variant="outline"
+          >
+            <RotateCw className="h-4" />
+            Limpar
+          </Button>
+        </div>
+
+        {videoUrlUnify && (
+          <video ref={videoRef} src={videoUrlUnify} controls hidden />
+        )}
+
+        {!!videosUrls.length && (
+          <div className="border rounded-md">
+            <div className="flex justify-between items-center p-2 border">
+              <p className="text-sm text-zinc-500 p-2 font-medium">Preview</p>
+              <Button
+                onClick={() => videoService.dowload(videosUrls)}
+                size="sm"
+                variant="outline"
+              >
+                <Download className="h-3" />
+                Download
+              </Button>
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
+              {videosUrls.map((url) => (
+                <video
+                  key={url}
+                  className="max-h-[150px] w-full object-contain"
+                  src={url}
+                  controls
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  )
+}
---
+++ src/hooks/useHome.tsx
@@ -0,0 +1,142 @@
+import { VideoService } from '@/services/VideoService'
+import { Scissors, Merge, Check, Play } from 'lucide-react'
+import { useState, useRef, ChangeEvent } from 'react'
+import { toast } from 'sonner'
+
+type Status = 'stop' | 'unify' | 'split' | 'ready'
+
+export function useHome(videoService: VideoService) {
+  const [files, setFiles] = useState<File[]>([])
+  const [videoUrlUnify, setVideoUrlUnify] = useState<string>()
+  const [videosUrls, setVideosUrls] = useState<string[]>([])
+  const [status, setStatus] = useState<Status>('stop')
+  const inputFile = useRef<HTMLInputElement | null>(null)
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+
+  function handleInput(event: ChangeEvent<HTMLInputElement>) {
+    try {
+      if (!event.target.files) return
+      const files = Array.from(event.target.files)
+      const incorretTypeFiles = files.some((file) => file.type !== 'video/mp4')
+
+      if (incorretTypeFiles) {
+        throw new Error('Somente arquivos do tipo video/mp4 são permitidos')
+      }
+
+      setFiles(files)
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(
+          `Ocorreu um problema ao iniciar o processo. (${error.message})`,
+        )
+      }
+    }
+  }
+
+  function sleep(time: number) {
+    return new Promise<boolean>((resolve) =>
+      setTimeout(() => resolve(true), time),
+    )
+  }
+
+  async function handleInit() {
+    try {
+      setStatus('unify')
+      const url = await videoService.unifyVideos(files)
+      setVideoUrlUnify(url)
+
+      await sleep(1000)
+      setStatus('split')
+
+      if (!videoRef.current) {
+        setStatus('stop')
+        return
+      }
+
+      const urls = await videoService.splitVideo(url, videoRef.current.duration)
+      setVideosUrls(urls)
+      setStatus('ready')
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(
+          `Ocorreu um problema ao iniciar o processo. (${error.message})`,
+        )
+      }
+    }
+  }
+
+  function handleResetForm() {
+    setStatus('stop')
+    if (inputFile.current) {
+      inputFile.current.value = ''
+    }
+    setVideoUrlUnify('')
+    setFiles([])
+    setVideosUrls([])
+  }
+
+  function getBgColor() {
+    if (status === 'split') {
+      return 'bg-blue-500'
+    }
+
+    if (status === 'unify') {
+      return 'bg-yellow-500'
+    }
+
+    if (status === 'ready') {
+      return 'bg-green-500'
+    }
+
+    return 'bg-primary'
+  }
+
+  function getLabel() {
+    if (status === 'split') {
+      return (
+        <div className="flex items-center gap-1">
+          <Scissors className="h-5" />
+          Editando...
+        </div>
+      )
+    }
+
+    if (status === 'unify') {
+      return (
+        <div className="flex items-center gap-1">
+          <Merge className="h-5" />
+          Unificando...
+        </div>
+      )
+    }
+
+    if (status === 'ready') {
+      return (
+        <div className="flex items-center gap-1">
+          <Check className="h-5" />
+          Pronto
+        </div>
+      )
+    }
+
+    return (
+      <div className="flex items-center gap-1">
+        <Play className="h-4" />
+        Iniciar
+      </div>
+    )
+  }
+
+  return {
+    handleInit,
+    handleInput,
+    getLabel,
+    getBgColor,
+    handleResetForm,
+    videoUrlUnify,
+    videosUrls,
+    inputFile,
+    videoRef,
+    files,
+  }
+}
